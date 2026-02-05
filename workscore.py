d8e2f73917a39c646f80b5d607912886f1a804a5
eunjeong <bluejle@naver.com>
2026-02-05 12:02:38 +0900

    점수변경

--- workscore.py
+++ workscore.py
@@ -5,8 +5,9 @@ kor,eng,hap,avg = 0,0,0,0
 info = ''
 message = ''
 
-kor = 90
-eng = 85
+kor = 30
+eng = 95
+avg=hap//2
 # 해결순서1] 국어,영어 점수 input()데이터입력받기, 형변환
 # 해결순서2] 총계, 평균 연산처리 
 # 해결순서3] 평균점수 70점부터 축합격 0~69점까지 재시험  if~else제어문 사용
@@ -15,6 +16,7 @@ print()
 print(f'국어={kor}')
 print(f'영어={eng}')
 print(f'평균={avg}')
+
 
 
 
